75ec5e1596e6bbaf638995a10d79bc97ac3bf4e6
pusto-tauranth <619980817@qq.com>
2017-02-22 18:58:24 +0800

    Initializing basic functions and database

--- app/src/main/java/com/lyz/dewordhelper/PlTrainingActivity.java
+++ app/src/main/java/com/lyz/dewordhelper/PlTrainingActivity.java
@@ -1,7 +1,9 @@
 package com.lyz.dewordhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PlTrainingActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class PlTrainingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pl_training);
         this.setTitle("复数训练");
+    }
+    public void onPlAClick(View v) {
+        if (true) {
+            Intent intent = new Intent(this, ReportActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
---
+++ app/src/main/java/com/lyz/dewordhelper/Words.java
@@ -0,0 +1,13 @@
+package com.lyz.dewordhelper;
+
+/**
+ * Created by 61998 on 2017/2/22.
+ */
+
+public class Words {
+    public static final String TABLE = "123";//表名
+    public static final String Key_ID="ID";
+    public static final String Key_gender="gender";
+    public static final String Key_word="word";
+    public static final String Key_pl="gender";
+}
